b798308a22abe714a311b3066c232082a3e8a357
leehongjung <leehongjungdaniel@gmail.com>
2025-07-09 18:47:07 +0700

    commit

---
+++ script/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function) {
+    
+    const DISCOUNT_CODE = "UIIAI" ;
+    const DISCOUNT_PRESENTAGE = 0.25;
+
+    $(".plus-btn").on("click",function(){
+        let quantitySpan = $(this)
+    })
+}
