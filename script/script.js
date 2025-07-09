710bbf61964664bad76cc9a37179b2d6684e5415
leehongjung <leehongjungdaniel@gmail.com>
2025-07-09 19:02:49 +0700

    commit

--- script/script.js
+++ script/script.js
@@ -13,5 +13,17 @@ $(document).ready(function) {
         let currentQuantity = parseInt(quantitySpan.text());
         if (currentQuantity > 0){quantitySpan.text(currentQuantity-1);
     }});
+
+    function calculateTotal() {
+        let total = 0;
+        $(".product-item").each(function()
+            let price = $(this).find(".price").data("price");
+            let quantity = parseInt($(this).find("quantity").text()));
+            total += price * quantity;
+        };
+        kode diskon tidak valid <br>
+        <strong>total bayar:$${totalOriginal.toFixed</strong>
+        
+};
     
-}
+    
