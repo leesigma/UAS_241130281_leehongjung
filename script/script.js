312800b4b6e573af06fe9de92981c31cb6ef426f
leehongjung <leehongjungdaniel@gmail.com>
2025-07-09 18:51:11 +0700

    commit

--- script/script.js
+++ script/script.js
@@ -4,6 +4,14 @@ $(document).ready(function) {
     const DISCOUNT_PRESENTAGE = 0.25;
 
     $(".plus-btn").on("click",function(){
-        let quantitySpan = $(this)
-    })
+        let quantitySpan = $(this).siblings(".quantity");
+        let currentQuantity = parseInt(quantitySpan.text());
+        if (currentQuantity > 0){quantitySpan.text(currentQuantity+1);
+    }});
+    $(".minus-btn").on("click",function(){
+        let quantitySpan = $(this).siblings(".quantity");
+        let currentQuantity = parseInt(quantitySpan.text());
+        if (currentQuantity > 0){quantitySpan.text(currentQuantity-1);
+    }});
+    
 }
